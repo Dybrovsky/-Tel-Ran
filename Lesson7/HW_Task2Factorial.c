8be84a06f4e3152b61efc2533c02e990834a2642
Dybrovsky <sceven71@gmail.com>
2022-10-19 18:32:52 +0300

    factorial

---
+++ Lesson7/HW_Task2Factorial.c
@@ -0,0 +1,33 @@
+// Task 2
+// 	Написать функцию int factorial(int number) для вычисления факториала целого числа в диапазоне [1 - 9], 
+//     диапазон должен задаваться пользователем через консоль, также, как и число для расчета факториала с использованием цикла FOR:
+
+// Output:
+//   5! = 120
+
+#include <stdio.h>
+int factorial(int number);
+int scan();
+  int main()
+  {
+     int number;
+     printf("Enter factorial\n");
+     number = scan();
+     factorial(number);
+  }
+
+  int factorial(int number){
+     int i;
+     float res = 1;
+      for( i = 1; i <= number; i++ )
+      res = i * res;
+          printf("%.f\n", res);
+      return 0;
+  }
+
+    int scan(){
+	int input;
+	fflush(stdout);
+	scanf("%d", &input);
+	return input;
+}
