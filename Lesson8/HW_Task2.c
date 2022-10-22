0dd4a2bda2711010ff365056f2a6d6a8e52f88c9
Dybrovsky <sceven71@gmail.com>
2022-10-22 18:54:14 +0300

    HW2

---
+++ Lesson8/HW_Task2.c
@@ -0,0 +1,40 @@
+// Task 2
+
+// Implements a function that will reverse array
+
+// void reverseArr(int arr[], int size);
+
+// Example:
+// int main(){
+//   int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//   reverseArr(arr,10);
+//   printArr(arr,10);//on console -> [10][9][8][7][6][5][4][3][2][1]
+//   return 0;
+// }
+
+#include<stdio.h>
+void reverseArr(int arr[], int size);
+
+int main(){
+
+  int arr[] = {1,2,3,4,5,6,7,8,9,10};
+  reverseArr(arr,10);
+
+
+    return 0;
+}
+
+void reverseArr(int arr[], int size){
+     int i;
+     int sizeArr = size;
+    int temp;
+
+    for(int i = 0; i<sizeArr/2; i++){
+        temp = arr[i];
+        arr[i] = arr[sizeArr-i-1];
+        arr[sizeArr-i-1] = temp;
+    }
+    for(int i = 0; i < sizeArr; i++){
+        printf("%d ", arr[i]);
+    }
+ }
