d554c8bed700ec5ff979b6051c90fd56501b9b4c
Dybrovsky <sceven71@gmail.com>
2022-10-27 15:07:29 +0300

    DigitTZJava

---
+++ Lesson11/MainDigit.java
@@ -0,0 +1,28 @@
+public class MainDigit {
+
+    public static void main(String[] args) {
+
+        int number = 43214321;
+        System.out.println("Your control digit is " + countDigit(number));
+
+    }
+
+    static int countDigit(int num) {
+
+        int count = 0, digit = 0, sum = 0, res = 0;
+        do {
+            digit = num % 10;
+            num /= 10;
+            if (count % 2 == 0)
+                digit *= 2;
+            if (digit > 9)
+                digit -= 9;
+            sum += digit;
+            count++;
+        } while (num > 0);
+        res = 10 - sum % 10;
+        return (res == 10) ? 0 : res;
+
+    }
+
+}
