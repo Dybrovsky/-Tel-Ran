d2b237b92dcc9768787f0bf23e6ab5b93424b6d2
Dybrovsky <sceven71@gmail.com>
2022-11-19 13:52:23 +0200

    Task2

---
+++ Lesson17/Task2.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Map<Character, Integer> letters = new HashMap<Character, Integer>();
+
+        ArrayList<Character> tempArray = new ArrayList<Character>();
+
+        tempArray.add('A');
+        tempArray.add('A');
+        tempArray.add('B');
+        tempArray.add('C');
+        tempArray.add('B');
+        tempArray.add('A');
+
+        for (var i = 0; i < tempArray.size(); i++) {
+            Character tempChar = tempArray.get(i);
+
+            if (!letters.containsKey(tempChar)) {
+                letters.put(tempChar, 1);
+            } else {
+                letters.put(tempChar, letters.get(tempChar) + 1);
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : letters.entrySet()) {
+            System.out.println("Буква = " + entry.getKey() + ", Повторений = " + entry.getValue());
+        }
+
+    }
+}
