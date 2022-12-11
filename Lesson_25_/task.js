d3169ade60f75825a0648408ccc630a5a51c4549
Dybrovsky <sceven71@gmail.com>
2022-12-11 15:53:45 +0200

    CSS_1

---
+++ Lesson_25_/task.js
@@ -0,0 +1,12 @@
+function maskify(cc) {
+
+    for(i = 0; i > cc.length; i++)
+        let res = "";
+        let el = cc[i] = "#";
+        res += res + el;
+
+        return res;
+}
+
+let a = "dhdhdhdhdhdhdh";
+console.log(maskify(a));
