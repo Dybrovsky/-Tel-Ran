e91174fc92bd77a16973301535046e6efe18d0b3
Dybrovsky <sceven71@gmail.com>
2022-12-23 17:55:19 +0200

    Js_HW

--- JS_1_HW/script.js
+++ JS_1_HW/script.js
@@ -1,3 +1,5 @@
+// Task 1
+
 function xStatement(a, b) {
     let x = b / a;
     let res;
@@ -7,7 +9,7 @@ function xStatement(a, b) {
     } else if (a == 0 && b == 0) {
         res = 'solution is any number';
     } else {
-        res =`x = ${x}`;
+        res = `x = ${x}`;
     }
     return res;
 
@@ -16,23 +18,54 @@ function xStatement(a, b) {
 console.log(xStatement(10, 0))
 
 
+
+// Task 3
+
+let Str = 'Qwerty';
+
+function reverseStr(string) {
+    return res = string.split('').reverse().join('');
+    
+}
+
+console.log(reverseStr(Str));
+
+
+// Task 2
+
+
 function calculation() {
     const operand1 = prompt('first operand?', '');
-    const operand2 = prompt('second operand?', '');
     const operator = prompt('operation', '');
-    switch (operator) {
-        case '-' : alert (operand1 - operand2);
-        break;
-        case '+' : alert (+operand1 + +operand2);
-        break;
-        case '*' : alert (operand1 * operand2);
-        break;
-        case '/' : alert (operand1 / operand2);
-        break;
-        
+    const operand2 = prompt('second operand?', '');
 
+    if (isNaN(operand1) || isNaN(operand2)) {
+        alert('Not a number!')
+    } else if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+        alert('Not an operation!')
+    } else {
+
+        switch (operator) {
+            case '-' :
+                alert(operand1 - operand2);
+                break;
+            case '+' :
+                alert(+operand1 + +operand2);
+                break;
+            case '*' :
+                alert(operand1 * operand2);
+                break;
+            case '/' :
+                if (operand2 == 0) {
+                    alert('Do not division on zero!!!')
+                    break;
+                } else {
+                    alert(operand1 / operand2);
+                }
+                break;
+        }
     }
 }
 
-calculation();
 
+calculation();
