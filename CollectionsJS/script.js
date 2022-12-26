67d9358d4c6fa08e82908408051179de5753f461
Dybrovsky <sceven71@gmail.com>
2022-12-26 19:51:07 +0200

    SetsCW

--- CollectionsJS/script.js
+++ CollectionsJS/script.js
@@ -1,15 +1,51 @@
-let name = prompt('Name');
-let secondName = prompt('second Name');
-let age = prompt('age');
 
 
 
 function Person(name, secondName, age) {
-    this.name = name;
-    this.secondName = secondName;
-    this.age = age;
+    this.name = name || 'no name';
+    this.secondName = secondName || 'no second name';
+    this.age = age || 0;
 }
 
-let person = new Person(name = prompt('Name'),secondName = prompt('second Name'), age = prompt('age'));
+let personArr = new Map();
+
+for (let i = 0; i < 5; i++) {
+    let name = prompt('Name');
+    let secondName = prompt('second Name');
+    let age = prompt('age');
+    let personID = new Person(name, secondName, age);
+    personArr.set(i, personID);
+}
 
 
+
+console.log(personArr);
+
+
+    let minAgeName, minAge = personArr.get(1).age;
+    let maxAgeName, maxAge = personArr.get(1).age;
+    let averageAge = 0;
+    
+    
+for (let i = 0; i < personArr.size; i++) {
+
+    let personAge = personArr.get(i).age;
+    
+    averageAge += +personAge;
+    
+    if (personAge < minAge) {
+        minAge = personAge;
+        minAgeName = personArr.get(i).name;
+        console.log()
+    }
+    
+    if (personAge > maxAge){
+        maxAge = personAge;
+        maxAgeName = personArr.get(i).name;
+
+    }
+}
+
+console.log(minAge, minAgeName);
+console.log(maxAge, maxAgeName);
+console.log(averageAge / personArr.size);
